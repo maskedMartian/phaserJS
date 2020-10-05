fd243c686392c579f2f0c7d2b6a336efd64f225f
travisTheRookie <travisb@fireflyint.com>
2020-10-04 20:03:14 -0400

    Module 4 complete - Learn Phaser Visual Effects...CodeCademy course complete

---
+++ code_cademy/4 - Learn Phaser Visual Effects/1 - Learn Phaser Cameras and Effects/2 - Platforms/game.js
@@ -0,0 +1,141 @@
+class Level extends Phaser.Scene {
+    constructor() {
+      super('Level')
+      this.heights = [4, 5]
+    }
+  
+    preload() {
+      this.load.image('platform', 'https://content.codecademy.com/courses/learn-phaser/Codey%20Tundra/platform.png');
+      this.load.image('snowflake', 'https://content.codecademy.com/courses/learn-phaser/Codey%20Tundra/snowflake.png');
+      this.load.spritesheet('campfire', 'https://content.codecademy.com/courses/learn-phaser/Codey%20Tundra/campfire.png',
+        { frameWidth: 32, frameHeight: 32});
+      this.load.spritesheet('codey', 'https://content.codecademy.com/courses/learn-phaser/Codey%20Tundra/codey.png', { frameWidth: 72, frameHeight: 90})
+  
+      this.load.image('bg1', 'https://content.codecademy.com/courses/learn-phaser/Codey%20Tundra/mountain.png');
+      this.load.image('bg2', 'https://content.codecademy.com/courses/learn-phaser/Codey%20Tundra/trees.png');
+      this.load.image('bg3', 'https://content.codecademy.com/courses/learn-phaser/Codey%20Tundra/snowdunes.png');
+    }
+  
+    create() {
+      gameState.active = true
+  
+      gameState.player = this.physics.add.sprite(125, 110, 'codey').setScale(.5);
+  
+      gameState.platforms = this.physics.add.staticGroup();
+  
+      this.createAnimations();
+      this.levelSetup();
+  
+      this.cameras.main.setBounds(0, 0, gameState.width, gameState.height);
+      this.physics.world.setBounds(0, 0, gameState.width, gameState.height + gameState.player.height);
+  
+      this.cameras.main.startFollow(gameState.player, true, 0.5, 0.5)
+      gameState.player.setCollideWorldBounds(true);
+  
+      this.physics.add.collider(gameState.player, gameState.platforms);
+  
+      gameState.cursors = this.input.keyboard.createCursorKeys();
+    }
+  
+    createPlatform(xIndex, yIndex) {
+      // Creates a platform evenly spaced along the two indices.
+      // If either is not a number it won't make a platform
+        if (typeof yIndex === 'number' && typeof xIndex === 'number') {
+          gameState.platforms.create((220 * xIndex),  yIndex * 70, 'platform').setOrigin(0, 0.5).refreshBody();
+        }
+    }
+  
+    createAnimations() {
+      this.anims.create({
+        key: 'run',
+        frames: this.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+      });
+  
+      this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('codey', { start: 4, end: 5 }),
+        frameRate: 10,
+        repeat: -1
+      });
+  
+      this.anims.create({
+        key: 'jump',
+        frames: this.anims.generateFrameNumbers('codey', { start: 2, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+      })
+  
+      this.anims.create({
+        key: 'fire',
+        frames: this.anims.generateFrameNumbers('campfire'),
+        frameRate: 10,
+        repeat: -1
+      })
+    }
+  
+    levelSetup() {
+      for (const [xIndex, yIndex] of this.heights.entries()) {
+        // call createPlatform here with xIndex and yIndex
+        this.createPlatform(xIndex, yIndex);
+      } 
+    }
+  
+    update() {
+      if(gameState.active){
+        if (gameState.cursors.right.isDown) {
+          gameState.player.flipX = false;
+          gameState.player.setVelocityX(gameState.speed);
+          gameState.player.anims.play('run', true);
+        } else if (gameState.cursors.left.isDown) {
+          gameState.player.flipX = true;
+          gameState.player.setVelocityX(-gameState.speed);
+          gameState.player.anims.play('run', true);
+        } else {
+          gameState.player.setVelocityX(0);
+          gameState.player.anims.play('idle', true);
+        }
+  
+        if (Phaser.Input.Keyboard.JustDown(gameState.cursors.space) && gameState.player.body.touching.down) {
+          gameState.player.anims.play('jump', true);
+          gameState.player.setVelocityY(-500);
+        }
+  
+        if (!gameState.player.body.touching.down){
+          gameState.player.anims.play('jump', true);
+        }
+  
+        if (gameState.player.y > gameState.height) {
+  
+        }
+      }
+    }
+  }
+  
+  const gameState = {
+    speed: 240,
+    ups: 380,
+    width: 2000,
+    height: 600,
+  };
+  
+  const config = {
+    type: Phaser.AUTO,
+    width: 500,
+    height: 600,
+    fps: {target: 60},
+    backgroundColor: "b9baff",
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 800 },
+        enableBody: true,
+  
+      }
+    },
+    scene: [Level]
+  };
+  
+  const game = new Phaser.Game(config);
+  
